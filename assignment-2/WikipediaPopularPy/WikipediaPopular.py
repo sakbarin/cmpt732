f9e03ee84567ee4ddf840afcb2237673c09fb025
Shahram Akbarinasaji <sakbarin@sfu.ca>
2019-09-18 21:32:28 -0700

    implement and test of WikipediaPopular.py completed.

--- assignment-2/WikipediaPopularPy/WikipediaPopular.py
+++ assignment-2/WikipediaPopularPy/WikipediaPopular.py
@@ -2,32 +2,40 @@ from pyspark import SparkConf, SparkContext
 import sys
 import re, string
 
+# columns orders in tuple
+VISIT_DATE = 0
+PAGE_LANG = 1
+PAGE_NAME = 2
+VISIT_COUNT = 3
+PAGE_SIZE = 4
+
+# input and output path
 inputs = sys.argv[1]
 output = sys.argv[2]
 
+# configuration
 conf = SparkConf().setAppName('WikipediaPopularPy')
 sc = SparkContext(conf=conf)
 
+# assert
 assert sys.version_info >= (3, 5)  # make sure we have Python 3.5+
 assert sc.version >= '2.3'  # make sure we have Spark 2.3+
 
-def separate_columns(line):
-	columns = line.split()
-	visit_date = columns[0]
-	page_lang = columns[1]
-	page_name = columns[2]
-	visit_count = columns[3]
-	page_size = columns[4]
-	yield (visit_date, page_lang, page_name, visit_count, page_size)
 
-def remove_unwantend_pages(page_info):
-	if (page_info[1].lower() == "en" and page_info[2] != "Main_Page" and page_info[2].startswith("Special:") == False):
-		return True
-	else:
-		return False
+def separate_columns(text_line):
+	columns = text_line.split()
+	yield (columns[VISIT_DATE], columns[PAGE_LANG], columns[PAGE_NAME], columns[VISIT_COUNT], columns[PAGE_SIZE])
+
+def cast_count_type(record):
+	return (record[VISIT_DATE], record[PAGE_LANG], record[PAGE_NAME], int(record[VISIT_COUNT], 10), record[PAGE_SIZE])
+
+def apply_constraints(page_info):
+	return (page_info[PAGE_LANG].lower() == "en"
+			and page_info[PAGE_NAME] != "Main_Page"
+			and page_info[PAGE_NAME].startswith("Special:") == False)
 
 def get_max(a,b):
-	if (int(a,10)>int(b,10)):
+	if (a[1] > b[1]):
 		return a
 	return b
 
@@ -35,15 +43,16 @@ def get_key(kv):
 	return kv[0]
 
 def tab_separated(kv):
-	return "%s \t %s" % (kv[0], kv[1])
+	return "%s \t (%s, %s)" % (kv[0], kv[1][1], kv[1][0])
 
 text = sc.textFile(inputs)
 
-page_visits = text.flatMap(separate_columns)
-result_pages = page_visits.filter(remove_unwantend_pages)
+init_records = text.flatMap(separate_columns)
+updated_records = init_records.map(cast_count_type)
+filtered_records = updated_records.filter(apply_constraints)
 
-pairs = result_pages.map(lambda x: (x[0], x[3]))
-max_result = pairs.reduceByKey(get_max)
+init_pairs = filtered_records.map(lambda x: (x[VISIT_DATE], (x[PAGE_NAME], x[VISIT_COUNT])))
+max_pairs = init_pairs.reduceByKey(get_max)
 
-outdata = max_result.sortBy(get_key).map(tab_separated)
+outdata = max_pairs.sortBy(get_key).map(tab_separated)
 outdata.repartition(1).saveAsTextFile(output)
