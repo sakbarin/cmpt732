bb26023ef9baa9dfa2d3cd225cf3b8ebf4410d6e
Shahram Akbarinasaji <sakbarin@sfu.ca>
2019-09-25 12:17:38 -0700

    wordcount-improved.py structure changed to SparkSkeletion.

--- assignment-3/WordCountImproved/wordcount-improved.py
+++ assignment-3/WordCountImproved/wordcount-improved.py
@@ -2,36 +2,43 @@ from pyspark import SparkConf, SparkContext
 import sys
 import re, string
 
-inputs = sys.argv[1]
-output = sys.argv[2]
+assert sys.version_info >= (3, 5) # make sure we have Python 3.5+
 wordsep = re.compile(r'[%s\s]+' % re.escape(string.punctuation))
 
-conf = SparkConf().setAppName('WordCountImproved')
-sc = SparkContext(conf=conf)
-
-assert sys.version_info >= (3, 5)  # make sure we have Python 3.5+
-assert sc.version >= '2.3'  # make sure we have Spark 2.3+
 
 def get_words(line):
 	words = wordsep.split(line)
 	for word in words:
 		yield (word.lower(), 1)
 
+
 def add(x, y):
 	return x + y
 
+
 def get_key(kv):
 	return kv[0]
+
 
 def output_format(kv):
 	k, v = kv
 	return '%s %i' % (k, v)
 
-text = sc.textFile(inputs)
 
-words = text.flatMap(get_words)
-filtered_words = words.filter(lambda x: x != "")
-word_count = filtered_words.reduceByKey(add)
+def main(inputs, output):
+	text = sc.textFile(inputs)
+	words = text.flatMap(get_words)
+	filtered_words = words.filter(lambda x: x != "")
+	word_count = filtered_words.reduceByKey(add)
+	outdata = word_count.sortBy(get_key).map(output_format)
+	outdata.saveAsTextFile(output)
 
-outdata = word_count.sortBy(get_key).map(output_format)
-outdata.saveAsTextFile(output)
+
+if __name__ == '__main__':
+	conf = SparkConf().setAppName('WordCountImprovedPy')
+	sc = SparkContext(conf=conf)
+	sc.setLogLevel('WARN')
+	assert sc.version >= '2.4'  # make sure we have Spark 2.4+
+	inputs = sys.argv[1]
+	output = sys.argv[2]
+	main(inputs, output)
