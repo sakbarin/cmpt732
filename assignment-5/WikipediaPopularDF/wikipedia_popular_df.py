052e92f14682ed5cc9a478ae89c0ce42ab7a19ee
Shahram Akbarinasaji <sakbarin@sfu.ca>
2019-10-09 20:59:11 -0700

    wikipedia popular using DataFrame implemented.

--- assignment-5/WikipediaPopularDF/wikipedia_popular_df.py
+++ assignment-5/WikipediaPopularDF/wikipedia_popular_df.py
@@ -3,58 +3,53 @@ assert sys.version_info >= (3, 5) # make sure we have Python 3.5+
 
 # initial config
 from pyspark.sql import SparkSession, functions, types
-spark = SparkSession.builder.appName('WeatherETL').getOrCreate()
+spark = SparkSession.builder.appName('WikpediaPopularDataFrame').getOrCreate()
 assert spark.version >= '2.4' # make sure we have Spark 2.4+
 spark.sparkContext.setLogLevel('WARN')
 sc = spark.sparkContext
 
 # constants schema
 wikipedia_schema = types.StructType([
-    types.StructField('page_language', types.StringType()),
-    types.StructField('page_title', types.StringType()),
-    types.StructField('visit_count', types.StringType()),
-    types.StructField('page_size', types.StringType()),
+    types.StructField('language', types.StringType()),
+    types.StructField('title', types.StringType()),
+    types.StructField('views', types.LongType()),
+    types.StructField('size', types.LongType()),
 ])
 
-# constants names
-LANG_COL = "language"
-TITLE_COL = "title"
-
-# constants values
-LANG_VAL = "en"
-MAIN_PAGE_VAL = "Main_Page"
-SPECIAL_PAGE_VAL = "Special:"
 
 @functions.udf(returnType=types.StringType())
 def path_to_hour(path):
     file_name_parts = path.split('/')
     file_name = file_name_parts[len(file_name_parts) - 1]
-    return file_name[11: len(file_name) - 3]
+    return file_name[11: len(file_name) - 7]
 
 
 def main(inputs, output):
     # read page visits
-    raw_data = spark.read.csv(inputs, sep=' ', schema=comments_schema).withColumn('filename', functions.input_file_name())
+    df_init = spark.read.csv(inputs, sep=' ', schema=wikipedia_schema).withColumn('filename', functions.input_file_name())
 
-    records_with_filename = raw_data.select('*', path_to_hour(raw_data['filename']).alias('datetime'))
+    # filter the pages we are interested in
+    f1_lang = df_init.filter(df_init['language'] == 'en')
+    f2_title = f1_lang.filter(f1_lang['title'] != ('Main_Page'))
+    f3_title = f2_title.filter(f2_title['title'].startswith('Special:') == False)
 
+    # select required columns
+    df_cols = f3_title.select(path_to_hour(f3_title['filename']).alias('hour'), f3_title['title'], f3_title['views']).cache()
 
-    # filter the pages we like
-    filter_1_lang = records.filter(records[LANGUAGE_COL] == 'en')
-    filter_2_main = filter_1_lang.filter(not f_en_pages[TITLE_COL].startswith(MAIN_PAGE_VAL) == False)
-    filter_3_spec = filter_2_main(f_main_page[TITLE_COL].startswith(SPECIAL_PAGE_VAL) == False)
+    # get hour and max of each hour
+    df_max = df_cols.groupBy(df_cols['hour']).agg(functions.max(df_cols['views']).alias('views'))
+    df_max_rename = df_max.select(df_max['hour'].alias('m_hour'), df_max['views'].alias('m_views'))
 
+    # broadcast df_max
+    df_max_bc = functions.broadcast(df_max_rename)
 
-    # group by
-    reddit_groups = comments_kv.groupBy(comments_kv['subreddit'])
+    # final join
+    df_joined = df_cols.join(df_max_bc, [df_cols['hour'] == df_max_bc['m_hour'], df_cols['views'] == df_max_bc['m_views']] , 'right_outer')
 
-    # find averages
-    reddit_averages = reddit_groups.agg(functions.avg(comments['score']))
-
-    reddit_averages.explain()
+    df_sorted = df_joined.sort(df_joined['hour'].desc()).select(df_joined['hour'], df_joined['title'], df_joined['views'])
 
     # write to output
-    reddit_averages.write.csv(output, mode='overwrite')
+    df_sorted.coalesce(1).write.json(output, compression='gzip', mode='overwrite')
 
 if __name__ == '__main__':
     inputs = sys.argv[1]
