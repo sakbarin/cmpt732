7190fd6365d916ded37a4b304e26e53ec8a09ce8
Shahram Akbarinasaji <sakbarin@sfu.ca>
2019-10-22 15:17:22 -0700

    load nasa logs to Cassandra. init empty code.

---
+++ assignment-7/LoadLogs/load_logs.py
@@ -0,0 +1,12 @@
+import sys
+
+
+def main(inputs, workspace, table):
+    pass
+
+if (__name__ == '__main__'):
+    inputs = sys.argv[1]
+    workspace = sys.argv[2]
+    table = sys.argv[3]
+
+    main(inputs, workspace, table)
